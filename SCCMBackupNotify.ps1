380eca912c0f174a81ca4ca3ee787ca91be3a4ce
tombymac0336 <32399595+tombymac0336@users.noreply.github.com>
2018-10-24 14:42:54 -0400

    Add files via upload

---
+++ SCCMBackupNotify.ps1
@@ -0,0 +1,8 @@
+﻿$msgBackup = Get-Content \\mscmcprdps01\Backup\backupstatus.txt | Select-Object -last 1  
+
+$DCO_email='dl-go-sql-server@lowes.com'
+$cc_emails='thomas.g.mcneil@lowes.com','thommy.mcneil@gmail.com','Josh.L.Abercrombie@lowes.com','Timothy.B.Hanson@lowes.com'
+$msgMailServer="OUTLOOK-USNCCORP01.lowes.com"
+$msgSubject="Update - SCCM Site Server Backup"
+$msgBody=  $msgBackup
+send-mailmessage -to $DCO_email -Cc $cc_emails -from $DCO_email -subject $msgSubject -body $msgBody -smtpServer $msgMailServer
